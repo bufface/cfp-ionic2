3bfa701587ab2db761591a6dd7966ea2584788c3
Cristian Buffa <buffa@gmail.com>
2016-11-07 21:45:17 -0300

    Video 42: Mostrar marcadores en el mapa

--- MoneyMap/src/pages/map/map.ts
+++ MoneyMap/src/pages/map/map.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {GoogleMap, GoogleMapsEvent, GoogleMapsLatLng} from 'ionic-native';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
 import { GeolocationService } from '../../services/geolocation.service';
+import { Transaction } from '../../database';
 
 /*
   Generated class for the Map page.
@@ -15,6 +16,8 @@ import { GeolocationService } from '../../services/geolocation.service';
 })
 export class Map {
 
+  map: GoogleMap = null;
+
   constructor(public navCtrl: NavController, public geolocator: GeolocationService) {}
 
   ionViewDidLoad() {
@@ -23,10 +26,30 @@ export class Map {
     }).catch((err) => console.log(err));
   }
 
+  loadMarkers() {
+    Transaction.all().then((resp) => this.loadTransactionMarkers(resp));
+  }
+
+  loadTransactionMarkers(transactions) {
+    for (var i = 0; i < transactions.length; ++i) {
+      let transaction = transactions[i];
+      let markerLocation: GoogleMapsLatLng = new GoogleMapsLatLng(transaction.lat, transaction.lng);
+      let markerOptions: GoogleMapsMarkerOptions = {
+        position: markerLocation,
+        title: transaction.title,
+        icon: 'blue'
+      };
+
+      this.map.addMarker(markerOptions).then((marker: GoogleMapsMarker) => {
+        marker.showInfoWindow();
+      }).catch((err) => console.log(err));
+    }
+  }
+
   loadMap(lat, lng) {
     let location: GoogleMapsLatLng = new GoogleMapsLatLng(lat, lng);
 
-    new GoogleMap('map', {
+    this.map = new GoogleMap('map', {
       'controls': {
         'compass': true,
         'myLocationButton': true,
@@ -46,5 +69,7 @@ export class Map {
         'bearing': 50
       }
     });
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => this.loadMarkers())
   }
 }
